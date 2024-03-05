0a45c1578e237b4898ef3143df806162a60d8ce8
J. Jefferson N. do Vale <jose.vale@compasso.com.br>
2024-03-04 23:55:29 -0300

    update insert db test complete

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,15 @@
---insert into tb_task (description, title) values ('Here one description', 'My first task of the project');
---insert into tb_task (description, title) values ('Here one description', 'My second task of the project');
+-- Insert users for tests
+insert into tb_user (first_name, last_name, email, department, created, last_update) values ('Jefferson','Nascimento', 'jefferson@dev.com', 1, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
+insert into tb_user (first_name, last_name, email, department, created, last_update) values ('David','Silva', 'david@dev.com', 1, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
+insert into tb_user (first_name, last_name, email, department, created, last_update) values ('Michael','Tomas', 'michal@dev.com', 2, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
+insert into tb_user (first_name, last_name, email, department, created, last_update) values ('Marcos','Vale', 'marcos@dev.com', 1, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
+insert into tb_user (first_name, last_name, email, department, created, last_update) values ('John','Barbosa', 'john@dev.com', 1, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
+insert into tb_user (first_name, last_name, email, department, created, last_update) values ('Maria','Geri', 'maria@dev.com', 2, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
+insert into tb_user (first_name, last_name, email, department, created, last_update) values ('Antonio','Developer', 'antonio@dev.com', 1, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
+insert into tb_user (first_name, last_name, email, department, created, last_update) values ('Victor','Silva', 'victor@dev.com', 1, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
+insert into tb_user (first_name, last_name, email, department, created, last_update) values ('Sophie','Tom', 'sophie@dev.com', 2, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
 
-insert into tb_user (first_name, last_name, email, department) values ('Jefferson','Nascimento', 'jefferson@dev.com', 1);
-insert into tb_user (first_name, last_name, email, department) values ('David','Silva', 'david@dev.com', 1);
-insert into tb_user (first_name, last_name, email, department) values ('Michael','Tomas', 'michal@dev.com', 2);
-insert into tb_user (first_name, last_name, email, department) values ('Marcos','Vale', 'marcos@dev.com', 1);
-insert into tb_user (first_name, last_name, email, department) values ('John','Barbosa', 'john@dev.com', 1);
-insert into tb_user (first_name, last_name, email, department) values ('Maria','Geri', 'maria@dev.com', 2);
-insert into tb_user (first_name, last_name, email, department) values ('Antonio','Developer', 'antonio@dev.com', 1);
-insert into tb_user (first_name, last_name, email, department) values ('Victor','Silva', 'victor@dev.com', 1);
-insert into tb_user (first_name, last_name, email, department) values ('Sophie','Tom', 'sophie@dev.com', 2);
+-- Insert tasks for test
+insert into tb_task (title, description, protocol_number, user_id, status, classification, created, last_update) values ('My first task of the project', 'Here a good description', '20240305.0001', 1, 1, 2, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
+insert into tb_task (title, description, protocol_number, user_id, status, classification, created, last_update) values ('My second task of the project', 'Here a good description', '20240305.0002', 1, 2, 1, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
+insert into tb_task (title, description, protocol_number, user_id, status, classification, created, last_update) values ('My third task of the project', 'Here a very good description', '20240305.0003', 1, 1, 2, '2024-03-05T00:00:00.000+00:00', '2024-03-05T00:00:00.000+00:00');
