c88b692bc73c03077cbb399594204bcb7bb33f5a
J. Jefferson N. do Vale <jose.vale@compasso.com.br>
2024-03-04 23:40:18 -0300

    update insert db test

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,12 @@
 --insert into tb_task (description, title) values ('Here one description', 'My first task of the project');
 --insert into tb_task (description, title) values ('Here one description', 'My second task of the project');
 
---insert into tb_user (name, email, task_id) values ('Dev first', 'dev@dev.com', 1);
---insert into tb_user (name, email, task_id) values ('Dev second', 'dev@dev.com', 1);
---insert into tb_user (name, email, task_id) values ('Dev third', 'dev@dev.com', 2);
+insert into tb_user (first_name, last_name, email, department) values ('Jefferson','Nascimento', 'jefferson@dev.com', 1);
+insert into tb_user (first_name, last_name, email, department) values ('David','Silva', 'david@dev.com', 1);
+insert into tb_user (first_name, last_name, email, department) values ('Michael','Tomas', 'michal@dev.com', 2);
+insert into tb_user (first_name, last_name, email, department) values ('Marcos','Vale', 'marcos@dev.com', 1);
+insert into tb_user (first_name, last_name, email, department) values ('John','Barbosa', 'john@dev.com', 1);
+insert into tb_user (first_name, last_name, email, department) values ('Maria','Geri', 'maria@dev.com', 2);
+insert into tb_user (first_name, last_name, email, department) values ('Antonio','Developer', 'antonio@dev.com', 1);
+insert into tb_user (first_name, last_name, email, department) values ('Victor','Silva', 'victor@dev.com', 1);
+insert into tb_user (first_name, last_name, email, department) values ('Sophie','Tom', 'sophie@dev.com', 2);
